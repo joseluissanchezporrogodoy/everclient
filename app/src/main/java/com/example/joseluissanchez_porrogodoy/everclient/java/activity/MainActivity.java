35d0725a8f5499b953af5c38ac31219f5dddeabb
joseluissanchez-porrogodoy <joseluis.sanchezporro.godoy@gmail.com>
2016-07-28 18:22:46 +0200

    Feature Login

---
+++ app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/java/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.joseluissanchez_porrogodoy.everclient.java.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.evernote.client.android.EvernoteSession;
+import com.evernote.client.android.login.EvernoteLoginFragment;
+import com.example.joseluissanchez_porrogodoy.everclient.R;
+
+public class MainActivity extends AppCompatActivity implements EvernoteLoginFragment.ResultCallback {
+
+    public static void launch(Activity activity) {
+        activity.startActivity(new Intent(activity, MainActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EvernoteSession.getInstance().authenticate(MainActivity.this);
+    }
+
+    @Override
+    public void onLoginFinished(boolean successful) {
+        Toast toast =
+                Toast.makeText(getApplicationContext(),
+                        "login done", Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+}
