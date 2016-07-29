bb1cc77c8ba6d46639024a4cee34b12157a396c7
joseluissanchez-porrogodoy <joseluis.sanchezporro.godoy@gmail.com>
2016-07-29 14:16:14 +0200

    Fix menu structure

--- app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/activity/MainActivity.java
+++ app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/activity/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.joseluissanchez_porrogodoy.everclient.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.evernote.client.android.EvernoteSession;
@@ -23,10 +29,6 @@ import com.evernote.edam.type.Note;
 import com.evernote.edam.type.NoteSortOrder;
 import com.evernote.edam.type.Notebook;
 import com.example.joseluissanchez_porrogodoy.everclient.R;
-
-
-import net.vrallev.android.task.TaskResult;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,8 +42,17 @@ public class MainActivity extends AppCompatActivity implements EvernoteLoginFrag
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         if (!EvernoteSession.getInstance().isLoggedIn()) {
             // LoginChecker will call finish
             return;
@@ -49,7 +60,29 @@ public class MainActivity extends AppCompatActivity implements EvernoteLoginFrag
       findNotes();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_alphabetics) {
+            return true;
+        }
+        if (id == R.id.action_edition) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onLoginFinished(boolean successful) {
         Toast toast =
@@ -58,6 +91,9 @@ public class MainActivity extends AppCompatActivity implements EvernoteLoginFrag
 
         toast.show();
     }
+
+
+
 
 
     protected void loadData() {
