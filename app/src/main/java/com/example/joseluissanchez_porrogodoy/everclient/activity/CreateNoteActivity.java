9bdc65d406aebbce7f483bee7237441314b690a8
joseluissanchez-porrogodoy <joseluis.sanchezporro.godoy@gmail.com>
2016-08-02 10:49:17 +0200

    Save Image methods

--- app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/activity/CreateNoteActivity.java
+++ app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/activity/CreateNoteActivity.java
@@ -45,7 +45,7 @@ public class CreateNoteActivity extends AppCompatActivity {
                 saveNote();
             }
         });
-       // recognitionHandWriting();
+        recognitionHandWriting();
     }
 
     private void saveNote(){
@@ -69,6 +69,126 @@ public class CreateNoteActivity extends AppCompatActivity {
             }
         });
     }
+    private void saveImageNote( final ImageData mImageData){
 
+        InputStream in = null;
+        final Note note = new Note();
+        note.setTitle(etTitle.getText().toString());
+        try {
+            // Hash the data in the image file. The hash is used to reference the file in the ENML note content.
+            in = new BufferedInputStream(new FileInputStream(mImageData.getPath()));
+            FileData data = new FileData(EvernoteUtil.hash(in), new File(mImageData.getPath()));
+
+            ResourceAttributes attributes = new ResourceAttributes();
+            attributes.setFileName(mImageData.getFileName());
+
+            // Create a new Resource
+            Resource resource = new Resource();
+            resource.setData(data);
+            resource.setMime(mImageData.getMimeType());
+            resource.setAttributes(attributes);
+
+            note.addToResources(resource);
+
+            // Set the note's ENML content
+
+        } catch (Exception e){
+
+        }
+        finally
+        {
+            if (in != null) {
+                try{
+                    in.close();
+                }catch (Exception e){
+
+                }
+
+            }
+        }
+        EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+        noteStoreClient.createNoteAsync(note, new EvernoteCallback<Note>() {
+            @Override
+            public void onSuccess(Note result) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                //mostrar mensaje de error
+
+            }
+        });
+
+
+
+    }
+    private void recognitionHandWriting(){
+
+        EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+
+        noteStoreClient.getResourceRecognitionAsync("3a9f1886-e7ae-44b7-bc7a-559d64fa58f9", new EvernoteCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] result) {
+                String s = new String(result);
+                int a = 0;
+            }
+
+            @Override
+            public void onException(Exception exception) {
+
+            }
+        });
+    }
+    public static class ImageData implements Parcelable {
+
+        private final String mPath;
+        private final String mFileName;
+        private final String mMimeType;
+
+        public ImageData(String path, String fileName, String mimeType) {
+            mPath = path;
+            mFileName = fileName;
+            mMimeType = mimeType;
+        }
+
+        public String getPath() {
+            return mPath;
+        }
+
+        public String getFileName() {
+            return mFileName;
+        }
+
+        public String getMimeType() {
+            return mMimeType;
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(mPath);
+            dest.writeString(mFileName);
+            dest.writeString(mMimeType);
+        }
+
+        public static final Creator<ImageData> CREATOR = new Creator<ImageData>() {
+            @Override
+            public ImageData createFromParcel(final Parcel source) {
+                return new ImageData(source.readString(), source.readString(), source.readString());
+            }
+
+            @Override
+            public ImageData[] newArray(final int size) {
+                return new ImageData[size];
+            }
+        };
+    }
 
 }
