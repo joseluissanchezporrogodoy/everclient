7c8cf6f186c59eda9942e3b0e334f0032458be80
joseluissanchez-porrogodoy <joseluis.sanchezporro.godoy@gmail.com>
2016-08-02 17:04:42 +0200

    Main change

--- app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/activity/MainActivity.java
+++ app/src/main/java/com/example/joseluissanchez_porrogodoy/everclient/activity/MainActivity.java
@@ -67,8 +67,7 @@ public class MainActivity extends AppCompatActivity implements EvernoteLoginFrag
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        //.setAction("Action", null).show();
+
                 Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
                 startActivityForResult(intent,REQUEST_CODE_ADD);
             }
